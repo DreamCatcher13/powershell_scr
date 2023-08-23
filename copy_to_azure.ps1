0f72f84ec4c4120248933ad03a4a83923c859c3b
DreamCatcher13 <gerey37ng@gmail.com>
2023-08-23 18:39:07 +0300

    added code for writing to log file

--- copy_to_azure.ps1
+++ copy_to_azure.ps1
@@ -8,12 +8,21 @@ $myNotebooks = @(
 $container = 'https://url_of_my_storage.blob.core.windows.net/my_container'
 
 $date = (Get-Date).AddDays(-1) #yesterday
+$copiedNotebooks = @() 
 
 # check last modified time
 foreach ($notebook in $myNotebooks) {
     if ((Get-Item $notebook).LastWriteTime -gt $date ) {
         $name = ($notebook -split '\\')[-1]
-        Write-Host "Copying $name to azure`n###################################"
+        $copiedNotebooks += $name
+        Write-Host "`nCopying $name to azure`n###################################"
         azcopy copy $notebook $container
     }
 }
+
+# writing logs
+$content=(Get-Date -Format "dddd, MMMM dd, yyyy") + ' -- copied files: '
+foreach ($f in $copiedNotebooks) { 
+    $content += $f + " " 
+}
+Add-Content -Path "C:\Users\user\Docs\AZcopy.log" -Value $content
