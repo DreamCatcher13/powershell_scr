a42ece3e96a819d1f19681bea2a9751e2db13d15
DreamCatcher13 <gerey37ng@gmail.com>
2024-02-29 12:48:48 +0200

    refactor: changed flow to archive + upload

--- copy_to_azure.ps1
+++ copy_to_azure.ps1
@@ -1,28 +1,31 @@
-# create an array of file paths
-$myNotebooks = @(
-    'C:\Users\user\Docs\notebook1\Main_first.one'
-    'C:\Users\user\Docs\notebook2\Main_python.one'
-    'C:\Users\user\Docs\notebook3\Main_golang.one'
-)
-
-$container = 'https://url_of_my_storage.blob.core.windows.net/my_container'
-
-$date = (Get-Date).AddDays(-1) #yesterday
-$copiedNotebooks = @() 
-
-# check last modified time
-foreach ($notebook in $myNotebooks) {
-    if ((Get-Item $notebook).LastWriteTime -gt $date ) {
-        $name = ($notebook -split '\\')[-1]
-        $copiedNotebooks += $name
-        Write-Host "`nCopying $name to azure`n###################################"
-        azcopy copy $notebook $container
-    }
+# variables
+$name = "notebooks-$(Get-Date -Format dd-MM-yyyy).zip"
+$container = "https://url_of_my_storage.blob.core.windows.net/my_container"
+$deleted = @()
+$archive = "C:\Users\user\Docs\Archive"
+$myNotebooks = @{
+    Path = "C:\Users\user\Docs\notebooks\*"
+    DestinationPath = "C:\Users\user\Docs\Archive\$name"
 }
 
-# writing logs
-$content=(Get-Date -Format "dddd, MMMM dd, yyyy") + ' -- copied files: '
-foreach ($f in $copiedNotebooks) { 
-    $content += $f + " " 
+Write-Host "`nDeleting more than 30 days old zips`n###################################"
+$old = (Get-ChildItem -Path $archive | Where-Object { $_.CreationTime -lt (Get-Date).AddDays(-30)})
+$old | ForEach-Object {
+    $deleted += $_.Name
+    Remove-Item $_ 
 }
+Write-Host "Deleted: $deleted"
+
+Write-Host "`nArchiving $name`n###################################"
+Compress-Archive @myNotebooks
+Write-Host "Archived" 
+
+Write-Host "`nCopying $name to Azure`n###################################"
+azcopy copy $myNotebooks.DestinationPath $container
+Write-Host "Copied"
+
+# write log
+$content=(Get-Date -Format "dddd, MMMM dd, yyyy") + ":`n`t-- copied files: $name`n`t-- deleted files: $deleted"
 Add-Content -Path "C:\Users\user\Docs\AZcopy.log" -Value $content
+
+# one day I will add try {} block
